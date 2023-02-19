70ace55f8e3dd00579c21d9916c51c824b14a553
Nathaniel Mason <nmason0204@gmail.com>
2023-02-18 22:33:23 -0800

    Added two test cases and alg code, tests seem to be passing so far

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -15,33 +15,72 @@ import arrow
 
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
-    """
-    Args:
-       control_dist_km:  number, control distance in kilometers
-       brevet_dist_km: number, nominal distance of the brevet
-           in kilometers, which must be one of 200, 300, 400, 600,
-           or 1000 (the only official ACP brevet distances)
-       brevet_start_time:  An arrow object
-    Returns:
-       An arrow object indicating the control open time.
-       This will be in the same time zone as the brevet start time.
-    """
-    #figure out hours, minutes, round minutes to nearest integer
-    
+   """
+   Args:
+      control_dist_km:  number, control distance in kilometers
+      brevet_dist_km: number, nominal distance of the brevet
+         in kilometers, which must be one of 200, 300, 400, 600,
+         or 1000 (the only official ACP brevet distances)
+      brevet_start_time:  An arrow object
+   Returns:
+      An arrow object indicating the control open time.
+      This will be in the same time zone as the brevet start time.
+   """
+   # To calculate opening times, use the maximum speed from table
+   # Based on the brevet_dist, will determine max speed
+   # Then, once have that speed, use the control_dist and speed to calculate hours and mins
+   # Then shift the brevet start time with those hour and min values
+   max_speed = 0
+   
+   if brevet_dist_km <= 200:
+      max_speed = 34
+   elif 200 < brevet_dist_km <= 400:
+      max_speed = 32
+   elif 400 < brevet_dist_km <= 600:
+      max_speed = 30
+   elif 600 < brevet_dist_km <= 1000:
+      max_speed = 28
+   elif 1000 < brevet_dist_km <= 1300:
+      max_speed = 26
 
-    return brevet_start_time.shift(hours=hours, minutes=minutes)
+   hours_value = control_dist_km // max_speed # Floor division to get only the hours amount
+   # Now, separate into hours and mins
+   mins_value = ((control_dist_km / max_speed) - hours_value) * 60 # Multiplying resulting fractional part by 60
+
+   return brevet_start_time.shift(hours=hours_value, minutes=mins_value)
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
-    """
-    Args:
-       control_dist_km:  number, control distance in kilometers
-          brevet_dist_km: number, nominal distance of the brevet
-          in kilometers, which must be one of 200, 300, 400, 600, or 1000
-          (the only official ACP brevet distances)
-       brevet_start_time:  An arrow object
-    Returns:
-       An arrow object indicating the control close time.
-       This will be in the same time zone as the brevet start time.
-    """
-    return arrow.now()
+   """
+   Args:
+      control_dist_km:  number, control distance in kilometers
+         brevet_dist_km: number, nominal distance of the brevet
+         in kilometers, which must be one of 200, 300, 400, 600, or 1000
+         (the only official ACP brevet distances)
+      brevet_start_time:  An arrow object
+   Returns:
+      An arrow object indicating the control close time.
+      This will be in the same time zone as the brevet start time.
+   """
+   # To calculate closing times, use the minimum speed
+   # Based on the brevet_dist, will determine min speed
+   # Then, once have that speed, use the control_dist and speed to calculate hours and mins
+   # Then shift the brevet start time with those hour and min values
+   min_speed = 0
+   
+   if brevet_dist_km <= 200:
+      min_speed = 15
+   elif 200 < brevet_dist_km <= 400:
+      min_speed = 15
+   elif 400 < brevet_dist_km <= 600:
+      min_speed = 15
+   elif 600 < brevet_dist_km <= 1000:
+      min_speed = 11.428
+   elif 1000 < brevet_dist_km <= 1300:
+      min_speed = 13.333
+
+   hours_value = control_dist_km // min_speed # Floor division to get only the hours amount
+   # Now, separate into hours and mins
+   mins_value = ((control_dist_km / min_speed) - hours_value) * 60 # Multiplying resulting fractional part by 60
+
+   return brevet_start_time.shift(hours=hours_value, minutes=mins_value)
--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -14,17 +14,33 @@ log = logging.getLogger(__name__)
 
 
 def test_brevet1():
-    start_time = arrow.get("2023-02-17 00:00", "YYYY-MM-DD HH:mm")
+    start_time = arrow.get("2023-02-18 08:00", "YYYY-MM-DD HH:mm")
     dist = 200
     checkpoints = {
                     0: (start_time, start_time.shift(hours=1)),
-                    50: (start_time.shift(hours=1, minutes=28), start_time.shift(hours=3.5)),
-                    150: (start_time.shift(), start_time.shift(hours=1)),
-                    200: (start_time.shift(), start_time.shift(hours=1)),
+                    50: (start_time.shift(hours=1, minutes=28), start_time.shift(hours=3, minutes=30)),
+                    150: (start_time.shift(hours=4, minutes=25), start_time.shift(hours=10)),
+                    200: (start_time.shift(hours=5, minutes=53), start_time.shift(hours=13, minutes=30)),
                     }
 
-    for km, time_tuple in checkpoint.items():
+    for km, time_tuple in checkpoints.items():
         checkpoint_open, checkpoint_close = time_tuple
         assert open_time(km, dist, start_time) == checkpoint_open
         assert close_time(km, dist, start_time) == checkpoint_close
 
+def test_brevet2():
+    start_time = arrow.get("2023-04-04 13:00", "YYYY-MM-DD HH:mm")
+    dist = 300
+    checkpoints = {
+                    0: (start_time, start_time.shift(hours=1)),
+                    60: (start_time.shift(hours=1, minutes=46), start_time.shift(hours=4)),
+                    120: (start_time.shift(hours=3, minutes=32), start_time.shift(hours=8)),
+                    180: (start_time.shift(hours=5, minutes=18), start_time.shift(hours=12)),
+                    240: (start_time.shift(hours=7, minutes=8), start_time.shift(hours=16)),
+                    300: (start_time.shift(hours=9), start_time.shift(hours=20)),
+                    }
+
+    for km, time_tuple in checkpoints.items():
+        checkpoint_open, checkpoint_close = time_tuple
+        assert open_time(km, dist, start_time) == checkpoint_open
+        assert close_time(km, dist, start_time) == checkpoint_close
